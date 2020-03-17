4bbae0fcf69edf1f413979e857a20e1f59e799d2
Your Brennan Predmore <brennnann@gmail.com>
2020-03-17 15:21:47 -0500

    update database in workbench

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name) VALUES ('cheese');
+INSERT INTO burgers (name) VALUES ('egg');
+INSERT INTO burgers (name, taste_Good) VALUES ('impossible', false);
+INSERT INTO burgers (name, taste_Good) VALUES ('veggy', false);
+INSERT INTO burgers (name) VALUES ('blt');
